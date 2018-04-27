e0faa1941890251b65007c68a727531bf8cc8822
Guest User <Guest@Epicodus-3A.local>
2018-04-27 11:35:25 -0700

    update main page styling, add edit/create functionality to product controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,23 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     render :show
   end
+  
+  def edit
+   @product = Project.find(params[:id])
+   render :edit
+ end
+
+ def update
+   @project = Project.find(params[:id])
+     if@project.update(project_params)
+       redirect_to projects_path
+     else
+       render :edit
+     end
+ end
+
+ private
+    def project_params
+      params.require(:project).permit(:title, :description, :technologies_used)
+    end
 end
